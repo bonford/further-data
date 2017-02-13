91383e108882959cfe1033c44761fb8018c66111
bonford <bonford@users.noreply.github.com>
2017-02-13 16:25:52 -0500

    built working httpservice unit test

--- src/app/http/http.service.spec.ts
+++ src/app/http/http.service.spec.ts
@@ -1,12 +1,27 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { HttpService } from './http.service';
+
+
 
 describe('HttpService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpService]
+      providers: [
+    MockBackend,
+    BaseRequestOptions,
+    {
+      provide: Http,
+      useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
+        return new Http(backendInstance, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    },
+    HttpService
+  ]
     });
   });
 
@@ -14,3 +29,4 @@ describe('HttpService', () => {
     expect(service).toBeTruthy();
   }));
 });
+
